50c1b3f505bdc77018cfc69b102deff5e9aa2f9d
徐立 <xuli@uniwyse.com>
2023-03-28 17:35:50 +0800

    more slice funcs

--- lodashLike/main.go
+++ lodashLike/main.go
@@ -7,11 +7,33 @@ import (
 	"lodashLike/slice"
 )
 
+var persons = []Person{
+	{Name: "A", Age: 12},
+	{Name: "AA", Age: 13},
+	{Name: "B", Age: 18},
+	{Name: "C", Age: 0},
+	{Name: "B", Age: 18},
+}
+
 func main() {
 	fmt.Println("Add()", slice.Add(Int64(1), 2, 3, 4))
-	fmt.Println("Add()", slice.Add("a", "b", "c"))
+	fmt.Println("Add()", slice.Add([]string{"a", "b", "c"}...))
 
-	fmt.Println("Sum()", slice.Sum(1, 2, 3, 4))
+	fmt.Println("Compact()", slice.Compact([]int{0, 1, 3, 5, 0.0}))
+	fmt.Println("Compact()", slice.Compact([]string{"a", "b", "", "ccc", "阿萨德飞", ""}))
+
+	fmt.Println("Difference()", slice.Difference([]string{"a", "b", "", "ccc", "阿萨德飞", ""}, []string{"a", "b"}))
+
+	fmt.Println("FilterBy()", slice.FilterBy(persons, func(item Person) bool {
+		return item.Name <= "B"
+	}))
+
+	fmt.Println("FindFirstIndex()", slice.FindFirstIndex(persons, Person{"B", 18}))
+	fmt.Println("FindFirstIndexBy()", slice.FindFirstIndexBy(persons, func(item Person) bool {
+		return item.Age == 18
+	}))
+
+	fmt.Println("Intersection()", slice.Intersection([]int{0, 1, 3, 5, 0.0}, []int{0, 2, 3, 6, 7, 0.0}))
 
 	fmt.Println("Map()", slice.Map([]string{"aa", "bb", "cc"}, func(s string) string {
 		return s + "_" + s
@@ -20,22 +42,13 @@ func main() {
 		return strconv.Itoa(s) + "_" + strconv.Itoa(s)
 	}))
 
-	fmt.Println("Compact()", slice.Compact([]int{0, 1, 3, 5, 0.0}))
-	fmt.Println("Compact()", slice.Compact([]string{"a", "b", "", "ccc", "阿萨德飞", ""}))
+	fmt.Println("Reverse()", slice.Reverse([]string{"a", "b", "", "ccc", "阿萨德飞", ""}))
 
-	fmt.Println("Compact2()", slice.Compact2([]int{0, 1, 3, 5, 0.0}))
+	fmt.Println("Sum()", slice.Sum(1, 2, 3, 4))
 
-	persons := []Person{
-		{Name: "A", Age: 12},
-		{Name: "AA", Age: 13},
-		{Name: "B", Age: 18},
-		{Name: "B", Age: 18},
-	}
 	fmt.Println("SumBy()", slice.SumBy(persons, "Age"))
 
 	fmt.Println("Uniq()", slice.Uniq(persons))
-
-	fmt.Println("FindFirstIndex()", slice.FindFirstIndex(persons, Person{"B", 18}))
 
 	fmt.Println("Uniq()", slice.Uniq(persons))
 }
--- lodashLike/slice/slice.go
+++ lodashLike/slice/slice.go
@@ -23,6 +23,88 @@ func Add[T Addable](x ...T) T {
 	return z
 }
 
+func Compact[T comparable](s []T) (r []T) {
+	r = make([]T, 0)
+	emp := new(T)
+	for _, v := range s {
+		if v == *emp {
+			continue
+		}
+		r = append(r, v)
+	}
+	return r
+}
+
+func Difference[T comparable](s1 []T, s2 []T) (r []T) {
+	r = make([]T, 0)
+	for _, v := range s1 {
+		if FindFirstIndex(s2, v) == -1 {
+			r = append(r, v)
+		}
+	}
+	return
+}
+
+func FilterBy[T any](s []T, f func(item T) bool) []T {
+	r := make([]T, 0)
+	for _, v := range s {
+		if f(v) {
+			r = append(r, v)
+		}
+	}
+	return r
+}
+
+func FindFirstIndex[T comparable](s []T, item T) (index int) {
+	index = -1
+	for i, v := range s {
+		if item == v {
+			return i
+		}
+	}
+	return
+}
+
+/*
+field must be comparable.
+*/
+func FindFirstIndexBy[T any](s []T, f func(item T) bool) (index int) {
+	index = -1
+	for i, v := range s {
+		if f(v) {
+			return i
+		}
+	}
+	return
+}
+
+func Intersection[T comparable](s1, s2 []T) (r []T) {
+	r = make([]T, 0)
+	for _, v := range s1 {
+		if FindFirstIndex(s2, v) > -1 {
+			r = append(r, v)
+		}
+	}
+	return Uniq(r)
+}
+
+func Map[T, F any](s []T, f func(t T) F) (r []F) {
+	r = make([]F, len(s))
+	for i, v := range s {
+		r[i] = f(v)
+	}
+	return r
+}
+
+func Reverse[T any](s []T) (r []T) {
+	l := len(s)
+	r = make([]T, l)
+	for i, v := range s {
+		r[l-i-1] = v
+	}
+	return
+}
+
 func Sum[T Num](x ...T) T {
 	var z T
 	for i := 0; i < len(x); i++ {
@@ -57,52 +139,6 @@ func SumBy[T any](s []T, field string) (r float64) {
 		r += value
 	}
 	return r
-}
-
-func Map[T, F any](s []T, f func(t T) F) (r []F) {
-	r = make([]F, len(s))
-	for i, v := range s {
-		r[i] = f(v)
-	}
-	return r
-}
-
-func Compact2[T any](s []T) (r []T) {
-	fmt.Println("----- Compact2")
-	if len(s) == 0 {
-		return s
-	}
-	r = make([]T, 0)
-	// empty := new(T)
-	/* for _, v := range s {
-		if *empty != v {
-			r = append(r, v)
-		}
-	} */
-
-	return r
-}
-
-func Compact[T comparable](s []T) (r []T) {
-	r = make([]T, 0)
-	emp := new(T)
-	for _, v := range s {
-		if v == *emp {
-			continue
-		}
-		r = append(r, v)
-	}
-	return r
-}
-
-func FindFirstIndex[T comparable](s []T, item T) (index int) {
-	index = -1
-	for i, v := range s {
-		if item == v {
-			return i
-		}
-	}
-	return
 }
 
 func Uniq[T comparable](s []T) (r []T) {
